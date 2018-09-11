0573e2f9ef0e8d3af009e302b64ae682f68981b5
Keenan <Keenanking555@gmail.com>
2018-09-11 12:13:15 -0600

    Added Marshmallows

--- src/Controller/monsterController.java
+++ src/Controller/monsterController.java
@@ -1,9 +1,22 @@
 package Controller;
 
+import model.MarshmallowMonster;
+
 public class monsterController
 {
+	//Data member section
+	//Declaration section
+	private MarshmallowMonster myMonster;
+	
+	//Constructors Initialize data members!
+	public monsterController()
+	{
+		myMonster = new MarshmallowMonster();
+	}
+	
+	
 	public void start()
 	{
-		
+		System.out.println(myMonster);
 	}
 }
